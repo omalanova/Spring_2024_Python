22c3df870a6aab0328482f7cd8910acd782f177b
omalanova <oyuna_eles@mail.ru>
2024-02-16 22:55:13 +0800

    codewars_part1

--- hw/hw2.py
+++ hw/hw2.py
@@ -13,10 +13,10 @@
 Если да, выведите на экран текст 'Четное', а иначе - 'Нечетное' """
 
 # number = int(input('Введи число: '))
-# if number % 2 == 0:
-#     print('Четное')
-# else:
+# if number % 2:
 #     print('Нечетное')
+# else:
+#     print('Четное')
 
 """Задание 2.3
 Напишите программу, которая проверяет является ли год високосным. 
@@ -25,10 +25,24 @@
 на 400, он также считается високосным (1200, 2000)
 """
 
+# С вложенными условиями
+# year = int(input())
+# if year%4 == 0:
+#     if year%100 == 0:
+#         if year%400 == 0:
+#             print('Високосный')
+#         else:
+#             print('Невисокосный')
+#     else:
+#         print('Високосный')
+# else:
+#     print('Невисокосный')
+
+# C логическими операторами
 # year = int(input('Введите год: '))
-# if (year % 4 == 0) | (year % 400 == 0):
+# if not year%4 and year%100 or not year%400:
 #     print('Високосный год')
-# elif (year % 4 != 0) | (year % 100 == 0):
+# else:
 #     print('Невисокосный год')
 
 """Задание 2.4
@@ -44,15 +58,38 @@
 (в формате str), производит заданное арифметическое действие и печатает 
 результат в формате: {num1} {operator) {num2) = {result}"""
 
-num1 = int(input('введите 1-е число: '))
-num2 = int(input('введите 2-е число: '))
-operator = input('введите оператор: ')
+# num1 = int(input('введите 1-е число: '))
+# num2 = int(input('введите 2-е число: '))
+# operator = input('введите оператор: ')
+#
+# if operator == '+':
+#     print(f'{num1} {operator} {num2} = ', num1 + num2)
+# if operator == '-':
+#     print(f'{num1} {operator} {num2} = ', num1 - num2)
+# if operator == '*':
+#     print(f'{num1} {operator} {num2} = ', num1 * num2)
+# if operator == '/':
+#     print(f'{num1} {operator} {num2} = ', num1 / num2)
 
-if operator == '+':
-    print(f'{num1} {operator} {num2} = ', num1 + num2)
-if operator == '-':
-    print(f'{num1} {operator} {num2} = ', num1 - num2)
-if operator == '*':
-    print(f'{num1} {operator} {num2} = ', num1 * num2)
-if operator == '/':
-    print(f'{num1} {operator} {num2} = ', num1 / num2)
+# import sys
+# num1 = int(input('Пожалуйста, введите первое число: '))
+# num2 = int(input('Пожалуйста, введите второе число: '))
+# operator = input('Пожалуйста, введите один из следующих операторов - \'+\', \'-\', \'/\', \'*\', \'%\': ')
+# result = 0
+# if num2 == 0 and operator == '/':
+    # try:
+    # result = num1 / num2
+    # except ZeroDivisionError:
+    #     print('На ноль делить нельзя!')
+    #     sys.exit()
+# elif operator == '+':
+#     result = num1 + num2
+# elif operator == '*':
+#     result = num1 * num2
+# elif operator == '-':
+#     result = num1 - num2
+# elif operator == '%':
+#     result = num1 % num2
+# else:
+#     result = num1/num2
+# print(f'{num1} {operator} {num2} = {result}')
