287b5f0d686837db6bd57a97e0490f721d73b4f8
omalanova <oyuna_eles@mail.ru>
2024-02-16 23:17:50 +0800

    codewars_part1

--- codewars/part1.py
+++ codewars/part1.py
@@ -9,7 +9,22 @@
 # 999  --> "999"
 # -100 --> "-100"
 
-def number_to_string(num):
-    return str(num)
+# def number_to_string(num):
+#     return str(num)
+#
+# print(number_to_string(123))
 
-print(number_to_string(123))
+# Jenny's secret message
+
+# Jenny has written a function that returns a greeting for a user.
+# However, she('s in love with Johnny, and would like to greet him '
+# 'slightly different. She added a special case to her function, but she made a mistake.)
+#
+# Can you help her?
+
+def greet(name):
+    if name == "Johnny":
+        return "Hello, my love!"
+    return "Hello, {name}!".format(name=name)
+
+print(greet('Johnny'))
