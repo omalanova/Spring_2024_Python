57c1ae0db23bf7abff1f491fb43fa1d648e3b14d
omalanova <oyuna_eles@mail.ru>
2024-02-17 21:32:29 +0800

    codewars_part1

--- codewars/part1.py
+++ codewars/part1.py
@@ -219,5 +219,56 @@ def digitize(n):
 
 # Write a function that returns the total surface area and volume of a box as an array: [area, volume]
 
-def get_size(w, h, d):
-    return [2 * (w * h + h * d + d * w), w * h * d]
+# def get_size(w, h, d):
+#     return [2 * (w * h + h * d + d * w), w * h * d]
+
+# Rock Paper Scissors
+# my solution
+#
+# def rps(p1, p2):
+#     if p1 == p2:
+#         return "Draw!"
+#     if p1 == "scissors":
+#         if p2 == "rock":
+#             return "Player 2 won!"
+#         else:
+#             return "Player 1 won!"
+#     if p1 == "rock":
+#         if p2 == "paper":
+#             return "Player 2 won!"
+#         else:
+#             return "Player 1 won!"
+#     if p1 == "paper":
+#         if p2 == "scissors":
+#             return "Player 2 won!"
+#         else:
+#             return "Player 1 won!"
+#
+# #best practice1
+#
+# def rps(p1, p2):
+#     beats = {'rock': 'scissors', 'scissors': 'paper', 'paper': 'rock'}
+#     if beats[p1] == p2:
+#         return "Player 1 won!"
+#     if beats[p2] == p1:
+#         return "Player 2 won!"
+#     return "Draw!"
+# #best practice2
+#
+# def rps(p1, p2):
+#     hand = {'rock':0, 'paper':1, 'scissors':2}
+#     results = ['Draw!', 'Player 1 won!', 'Player 2 won!']
+#     return results[hand[p1] - hand[p2]]
+#
+# #best practice3
+#
+# def rps(p1, p2):
+#     if p1 == p2:
+#         return 'Draw!'
+#     elif (p1 == 'rock' and p2 == 'scissors') or (p1 == 'scissors' and p2 == 'paper') or (
+#             p1 == 'paper' and p2 == 'rock'):
+#         return 'Player 1 won!'
+#     else:
+#         return 'Player 2 won!'
+
+
