ac8f16c3b3d0ca8848e8efd9d595efce6493778b
omalanova <oyuna_eles@mail.ru>
2024-02-23 12:48:52 +0800

    mir_hw1

--- codewars/mir_hw1.py
+++ codewars/mir_hw1.py
@@ -10,6 +10,26 @@
 #     1 Imperial Gallon = 4.54609188 litres
 #     1 Mile = 1.609344 kilometres
 
-def converter(mpg):
-    return round(mpg / 4.54609188 * 1.609344, 2) # kpl
-print(converter(10))
+# def converter(mpg):
+#     return round(mpg / 4.54609188 * 1.609344, 2) # kpl
+# print(converter(10))
+
+# Lario and Muigi Pipe Problem
+
+# Issue
+#
+# Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+#
+# The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+#
+# The pipes are correct when each pipe after the first is 1 more than the previous one.
+# Task
+#
+# Given a list of unique numbers sorted in ascending order, return a new list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+# Example
+#
+# Input:  1,3,5,6,7,8 Output: 1,2,3,4,5,6,7,8
+
+def pipe_fix(nums):
+    return list(range(min(nums), max(nums) + 1))
+print(pipe_fix([6, 9]))
