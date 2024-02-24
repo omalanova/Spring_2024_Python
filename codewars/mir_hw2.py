44509ce5db2780ea929e438cd2138620df6c0292
omalanova <oyuna_eles@mail.ru>
2024-02-24 22:57:20 +0800

    mir_hw2

--- codewars/mir_hw2.py
+++ codewars/mir_hw2.py
@@ -58,3 +58,21 @@
 def cookie(x):
     return f'Who ate the last cookie? It was {"Zach" if type(x) is str else "Monica" if type(x) in [int, float] else "the dog"}!'
 print(cookie(2.3))
+
+# Filter the number
+# Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+# Task
+#
+# Your task is to return a number from a string.
+# Details
+#
+# You will be given a string of numbers and letters mixed up,
+# you have to return all the numbers in that string in the order they occur.
+
+def filter_string(st):
+    num = ''
+    for i in st:
+        if i.isdigit():
+            num += i
+    return int(num)
+
