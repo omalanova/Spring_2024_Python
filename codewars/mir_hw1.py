4db9dfd6dff7db84f59b776eb943d61e265cb245
omalanova <oyuna_eles@mail.ru>
2024-02-24 15:03:03 +0800

    mir_hw1

--- codewars/mir_hw1.py
+++ codewars/mir_hw1.py
@@ -184,9 +184,28 @@
 # MakeUpperCase
 # Write a function which converts the input string to uppercase.
 
-def make_upper_case(s):
-    # s1 = s.upper()
-    # return s1
-    return s.upper()
+# def make_upper_case(s):
+#     # s1 = s.upper()
+#     # return s1
+#     return s.upper()
+#
+# print(make_upper_case('hello'))
 
-print(make_upper_case('hello'))
+# How good are you really?
+# There was a test in your class and you passed it. Congratulations!
+# But you're an ambitious person. You want to know if you're better than the average student in your class.
+#
+# You receive an array with your peers' test scores. Now calculate the average and compare your score!
+#
+# Return True if you're better, else False!
+# Note:
+#
+# Your points are not included in the array of your class's points.
+# For calculating the average point you may add your point to the given array!
+
+def better_than_average(class_points, your_points):
+    average_of_class = sum(class_points) / len(class_points)
+    return your_points >= average_of_class
+print(better_than_average([2, 3], 5))
+print(better_than_average([100, 40, 34, 57, 29, 72, 57, 88], 75))
+print(better_than_average([41, 75, 72, 56, 80, 82, 81, 33], 50))
