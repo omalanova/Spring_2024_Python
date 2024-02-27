61eda700da189a74ae97696d9819b2a1bce9335a
omalanova <oyuna_eles@mail.ru>
2024-02-27 20:36:13 +0800

    mir_hw4

--- codewars/mir_hw3.py
+++ codewars/mir_hw3.py
@@ -71,3 +71,32 @@
 #     }[day]
 #
 # print(am_I_afraid('Monday', 13))
+
+# Jaden Casing Strings
+# Jaden Smith, the son of Will Smith, is the star of films such as The Karate Kid (2010) and After Earth (2013). Jaden is also known for some of his philosophy that he delivers via Twitter. When writing on Twitter, he is known for almost always capitalizing every word. For simplicity, you'll have to capitalize each word, check out how contractions are expected to be in the example below.
+#
+# Your task is to convert strings to how they would be written by Jaden Smith. The strings are actual quotes from Jaden Smith, but they are not capitalized in the same way he originally typed them.
+#
+# Example:
+#
+# Not Jaden-Cased: "How can mirrors be real if our eyes aren't real"
+# Jaden-Cased:     "How Can Mirrors Be Real If Our Eyes Aren't Real"
+
+# def to_jaden_case(string):
+#     jaden_string = ''
+#     sentence = string.split()
+#     for word in sentence:
+#         if "'" in word:
+#             jaden_string = jaden_string + word.capitalize() + ' '
+#         else:
+#             jaden_string = jaden_string + word.title() + ' '
+#     return jaden_string.strip()
+
+# best practice
+def to_jaden_case(string):
+    return ' '.join(word.capitalize() for word in string.split())
+
+# import string
+# toJadenCase = string.capwords
+
+print(to_jaden_case("How can mirrors be real if our eyes aren't real"))
