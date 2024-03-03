105b946ed91ecd866cb3b1ae6260adb3d5d8cc59
omalanova <oyuna_eles@mail.ru>
2024-03-03 16:55:19 +0800

    mir_hw5 Building blocks

--- codewars/mir_hw5.py
+++ codewars/mir_hw5.py
@@ -8,14 +8,64 @@
 #
 # ghost = Ghost()
 # ghost.color  #=> "white" or "yellow" or "purple" or "red"
+#
+# import random
+# class Ghost(object):
+#     def __init__(self):
+#         self.color = random.choice(['white', 'yellow', 'purple', 'red'])
+# ghost1 = Ghost()
+# print(ghost1.color)
 
-import random
-class Ghost(object):
-    def __init__(self):
-        self.color = random.choice(['white', 'yellow', 'purple', 'red'])
-ghost1 = Ghost()
-print(ghost1.color)
+# Building blocks
+# Write a class Block that creates a block (Duh..)
+# Requirements
+#
+# The constructor should take an array as an argument, this will contain 3 integers of the form [width, length, height] from which the Block should be created.
+#
+# Define these methods:
+#
+# `get_width()` return the width of the `Block`
+#
+# `get_length()` return the length of the `Block`
+#
+# `get_height()` return the height of the `Block`
+#
+# `get_volume()` return the volume of the `Block`
+#
+# `get_surface_area()` return the surface area of the `Block`
 
+class Block:
+    def __init__(self, array):
+        self.width = array[0]
+        self.length = array[1]
+        self.height = array[2]
+    def get_width(self):
+        return self.width
+    def get_length(self):
+        return self.length
+    def get_height(self):
+        return self.height
+    def get_volume(self):
+        return self.get_width() * self.get_length() * self.get_height()
+    def get_surface_area(self):
+        return 2 * (self.get_width() * self.get_length() + self.get_width() * self.get_height() + self.get_length() * self.get_height())
+
+# best practice
+#class Block(object):
+    # def __init__(self, wlh):
+    #     self.w, self.l, self.h = w,l,h = wlh
+    #     self.v = w*h*l
+    #     self.a = 2 * (w*l + w*h + l*h)
+    #
+    # def get_width(self):        return self.w
+    # def get_length(self):       return self.l
+    # def get_height(self):       return self.h
+    # def get_volume(self):       return self.v
+    # def get_surface_area(self): return self.a
+
+block1 = Block([2,2,2])
+print(block1.get_width(), block1.get_length(), block1.get_height())
+print(block1.get_volume(), block1.get_surface_area())
 
 # Menu Display
 # Create a class that imitates a select screen. The cursor can move to left or right!
--- hw/hw5.py
+++ hw/hw5.py
@@ -1,7 +1,26 @@
 # 5.1. Создайте любой класс с произвольным количеством подклассов, экземпляров, атрибутов и методов
 #     - как минимум один атрибут должен быть с уровнем доступа private.
 #     Соответственно, для получения значений этого атрибута нужно использовать методы get и set
+class Plant:
+    def __init__(self, type, name, area, __industrial_use):
+        self.type = type
+        self.name = name
+        self.area = area
+        self.__industrial_use = __industrial_use
 
+    def hello(self):
+        return f'{self.name} belongs to the {self.type} and grows in the {self.area}'
 
+    # @property
+    # def cultivation(self, growth):
+    #     self.growth = growth
+    #     return f'Cultivation of {self.name} is {self.growth}'
+
+plant1 = Plant('flower', 'rose', 'Europe', False)
+plant2 = Plant('conifers', 'pine', 'North hemisphere', True)
+
+print(plant1.hello())
+print(plant2.hello())
+# print(plant2.cultivation)
 
 # 5.2. Cоздайте репозиторий на Github и отправте файл с домашним заданием в этот удаленный репозиторий
