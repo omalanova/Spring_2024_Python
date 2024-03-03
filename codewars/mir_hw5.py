49d8882614ac14f30308d64a24207a2108e55ef0
omalanova <oyuna_eles@mail.ru>
2024-03-03 22:31:48 +0800

    mir_hw5 Thinkful - Object Drills: Quarks

--- codewars/mir_hw5.py
+++ codewars/mir_hw5.py
@@ -169,17 +169,66 @@
 # Titanic.is_worth_it()
 # False
 
-class Ship:
-    def __init__(self, draft, crew):
-        self.draft = draft
-        self.crew = crew
-    def is_worth_it(self):
-        return self.draft - 1.5 * self.crew > 20
+# class Ship:
+#     def __init__(self, draft, crew):
+#         self.draft = draft
+#         self.crew = crew
+#     def is_worth_it(self):
+#         return self.draft - 1.5 * self.crew > 20
+#
+# Titanic = Ship(15, 10)
+# print(Titanic.is_worth_it())
+# worthy_ship = Ship(100,20)
+# print(worthy_ship.is_worth_it())
 
-Titanic = Ship(15, 10)
-print(Titanic.is_worth_it())
-worthy_ship = Ship(100,20)
-print(worthy_ship.is_worth_it())
+# Thinkful - Object Drills: Quarks
+# You're modelling the interaction between a large number of quarks and have decided to create a Quark class so you can generate your own quark objects.
+#
+# Quarks are fundamental particles and the only fundamental particle to experience all four fundamental forces.
+# Your task
+#
+# Your Quark class should allow you to create quarks of any valid color ("red", "blue", and "green") and any valid flavor ('up', 'down', 'strange', 'charm', 'top', and 'bottom').
+#
+# Every quark has the same baryon_number (BaryonNumber in C#): 1/3.
+#
+# Every quark should have an .interact() (.Interact() in C#) method that allows any quark to interact with another quark via the strong force. When two quarks interact they exchange colors.
+# Example
+#
+# >>> q1 = Quark("red", "up")
+# >>> q1.color
+# "red"
+# >>> q1.flavor
+# "up"
+# >>> q2 = Quark("blue", "strange")
+# >>> q2.color
+# "blue"
+# >>> q2.baryon_number
+# 0.3333333333333333
+# >>> q1.interact(q2)
+# >>> q1.color
+# "blue"
+# >>> q2.color
+# "red"
+
+class Quark(object):
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+        self.baryon_number = 1 / 3
+    def interact(self, quark):
+        self.color, quark.color = quark.color, self.color
+
+q1 = Quark("red", "up")
+print(q1.color)
+print(q1.flavor)
+q2 = Quark("blue", "strange")
+print(q2.color)
+print(q2.flavor)
+print(q2.baryon_number)
+q1.interact(q2)
+print(q1.color)
+print(q2.color)
+
 
 # Menu Display
 # Create a class that imitates a select screen. The cursor can move to left or right!
