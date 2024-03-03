ae5473e4738e6ed2f0186229b4a75aea260d706b
omalanova <oyuna_eles@mail.ru>
2024-03-03 23:49:44 +0800

    mir_hw5 Refactored Greeting

--- codewars/mir_hw5.py
+++ codewars/mir_hw5.py
@@ -188,17 +188,17 @@
 #
 # Look tests for more examples, good luck :)
 
-class Class:
-    number = 1
-    @staticmethod
-    def get_number():
-        if Class.number == 1:
-            Class.number *= 2
-            return 1
-        else:
-            number_new = Class.number
-            Class.number *= 2
-            return number_new
+# class Class:
+#     number = 1
+#     @staticmethod
+#     def get_number():
+#         if Class.number == 1:
+#             Class.number *= 2
+#             return 1
+#         else:
+#             number_new = Class.number
+#             Class.number *= 2
+#             return number_new
 
 # best practice
 # class Class:
@@ -208,10 +208,10 @@ class Class:
 #         Class.value*=2
 #         return result
 
-Class.get_number() # -> 1
-Class.get_number() # -> 2
-Class.get_number() # -> 4
-print(Class.get_number()) # -> 8
+# Class.get_number() # -> 1
+# Class.get_number() # -> 2
+# Class.get_number() # -> 4
+# print(Class.get_number()) # -> 8
 
 # Thinkful - Object Drills: Quarks
 # You're modelling the interaction between a large number of quarks and have decided to create a Quark class so you can generate your own quark objects.
@@ -260,6 +260,29 @@ print(Class.get_number()) # -> 8
 # q1.interact(q2)
 # print(q1.color)
 # print(q2.color)
+
+# Refactored Greeting
+# The following code could use a bit of object-oriented artistry. While it's a simple method and works just fine as it is, in a larger system it's best to organize methods into classes/objects. (Or, at least, something similar depending on your language)
+#
+# Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
+#
+# Here is how the final refactored code would be used:
+#
+# joe = Person('Joe')
+# joe.greet('Kate') # should return 'Hello Kate, my name is Joe'
+# joe.name          # should == 'Joe'
+
+class Person:
+    def __init__(self, my_name):
+        self.name = my_name
+
+    def greet(self, your_name):
+        return f"Hello {your_name}, my name is {self.name}"
+
+jack = Person("Jack")
+jill = Person("Jill")
+print(jack.greet('Jill'))
+print(jack.greet('Mary'))
 
 
 # Menu Display
