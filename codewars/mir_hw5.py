983831cfda2c455b5f25e604e2fc95bf11a34d80
omalanova <oyuna_eles@mail.ru>
2024-03-04 23:12:06 +0800

    mir_hw5 These are not my grades

--- codewars/mir_hw5.py
+++ codewars/mir_hw5.py
@@ -317,6 +317,21 @@ class Student:
 
     def get_average(self):
         return sum(self.grades) / len(self.grades)
+
+# best practice
+# class Student:
+#
+#     def __init__(self, first_name, last_name, grades=None):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#         self.grades = grades or []
+#
+#     def add_grade(self, grade):
+#         self.grades.append(grade)
+#
+#     def get_average(self):
+#         return sum(self.grades) / len(self.grades)
+
 johnDoe = Student('John', 'Doe')
 janeDoe = Student('Jane', 'Doe')
 jamesSmith = Student('James', 'Smith')
