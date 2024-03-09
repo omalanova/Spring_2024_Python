5ca08b7dd4dfefbd2916223f5592794a001b8123
omalanova <oyuna_eles@mail.ru>
2024-03-09 15:32:49 +0800

    methods_of_work_with_strings Duplicate Encoder

--- codewars/methods_for_work_with_strings.py
+++ codewars/methods_for_work_with_strings.py
@@ -88,7 +88,27 @@
 # 'world'  =>  'dlrow'
 # 'word'   =>  'drow'
 
-def solution(string):
-    return string[::-1]
+# def solution(string):
+#     return string[::-1]
+#
+# print(solution('world'))
 
-print(solution('world'))
+# Duplicate Encoder
+# The goal of this exercise is to convert a string to a new string
+# where each character in the new string is "(" if that character appears only once in the original string, or ")" if that character appears more than once in the original string. Ignore capitalization when determining if a character is a duplicate.
+# Examples
+#
+# "din"      =>  "((("
+# "recede"   =>  "()()()"
+# "Success"  =>  ")())())"
+# "(( @"     =>  "))(("
+#
+# Notes
+#
+# Assertion messages may be unclear about what they display in some languages. If you read "...It Should encode XXX",
+# the "XXX" is the expected result, not the input!
+
+def duplicate_encode(word):
+    return ''.join('(' if word.lower().count(ch) == 1 else ')' for ch in word.lower())
+print(duplicate_encode("din"))
+print(duplicate_encode("Success"))
